f3fdc6661e2b50c68189315fe1fd66a063d04078
Marc Lopez <marclopeztorrent@gmail.com>
2021-12-15 14:09:34 +0100

    created register validator and added to the router and controller

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,45 +1,22 @@
 import { check } from 'express-validator'
 
-// app.post(
-//     '/user',
-//     // username must be an email
-//     body('username').isEmail(),
-//     // password must be at least 5 chars long
-//     body('password').isLength({ min: 5 }),
-//     (req, res) => {
-//       // Finds the validation errors in this request and wraps them in an object with handy functions
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//       }
-  
-//       User.create({
-//         username: req.body.username,
-//         password: req.body.password,
-//       }).then(user => res.json(user));
-//     },
-//   );
-
-
 export const registerValidator = (props) => {
     return [
         check("email")
-            // .notEmpty().trim()
-            // .withMessage(`Email is required`)
+            .notEmpty().trim()
+            .withMessage(`Email is required`)
             .isEmail().normalizeEmail()
-            .withMessage(`Must be a valid email`)
+            .withMessage(`Must be a valid email`),
+        check("firstname")
+            .notEmpty().trim()
+            .withMessage(`First name is required`),
+        check("lastname")
+            .notEmpty().trim()
+            .withMessage(`Last name is required`),
+        check("password")
+            .notEmpty().trim()
+            .withMessage(`Password is required`)
+            .isLength({ min: 8 })
+            .withMessage(`Email must be at least 8 characters long`)
     ]   
-    // let checks = [];
-    
-    // // "login" --> props = ["username", "password"]
-    // // "albums" -> props = ["band", "title", "year"]
-    // for (let field of props) {
-    //     checks.push(
-    //         check(field)
-    //         .notEmpty().trim()
-    //         .withMessage(`${field} is required`)
-    //     )
-    // }
-
-    // return checks;
 }
--- resources/user/user.router.js
+++ resources/user/user.router.js
@@ -1,13 +1,10 @@
 import { createUser, userLogin, verifyUser } from "./user.controllers.js";
 import express from "express";
 import {registerValidator} from '../../middleware/validators.js'
-import { check,body } from 'express-validator'
 
 const router = express.Router();
 
-// router.post("/", registerValidator(["email","firstname"]), createUser);
-router.post("/", check('email', 'Your email is not valid').not().isEmpty().isLength({min: 5}), createUser);
-
+router.post("/", registerValidator(["email","firstname", "lastname", "password"]), createUser); 
 router.get("/:userId", userLogin);
 router.get("/verify-email/:userId/:token", verifyUser)
 
