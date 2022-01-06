6244c284f7372080ede91284f5db04a59c5de454
Janowitz <janhapple@outlook.de>
2022-01-06 10:50:47 +0100

    fixing bugs in the code

--- resources/user/user.router.js
+++ resources/user/user.router.js
@@ -1,20 +1,5 @@
 import { createUser, userLogin, verifyUser, updateUser, deleteUser } from "./user.controllers.js";
 import express from "express";
-<<<<<<< HEAD
-import { registerValidator } from "../../middleware/validators.js";
-
-const router = express.Router();
-
-router.post(
-  "/",
-  registerValidator(["email", "firstname", "lastname", "password"]),
-  createUser
-);
-router.post("/login", userLogin);
-router.get("/verify-email/:userId/:token", verifyUser);
-
-// router.get("/user-signed-up/")
-=======
 import { registerValidator, loginValidator} from '../../middleware/validators.js'
 
 const router = express.Router();
@@ -24,6 +9,5 @@ router.get("/:userId", loginValidator(["email", "password"]), userLogin);
 router.get("/verify-email/:userId/:token", verifyUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
->>>>>>> eae171afad7a0bc9b3155663a44673fe63183bc1
 
 export default router;
