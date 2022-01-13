24ac4469850523fa9ba81f2fc2b4dd822b100837
Edith-Heidmann <eheidmann@gmail.com>
2022-01-13 12:42:17 +0100

    Updated amenitites list

--- resources/location/location.model.js
+++ resources/location/location.model.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose'
-import { geocoder } from '../../middleware/geocoder.js'
+import mongoose from "mongoose";
+import { geocoder } from "../../middleware/geocoder.js";
 
-const locationSchema = new mongoose.Schema({
-    name: { 
-        type: String,
-        required: true,
-        unique: true 
+const locationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    // description: { 
+    // description: {
     //     type: String,
     //     required: true
     // },
@@ -22,20 +23,19 @@ const locationSchema = new mongoose.Schema({
     // },
     address: {
       type: String,
-      required: [true, 'Please add an address']
+      required: [true, "Please add an address"],
     },
     location: {
       type: {
         type: String,
-        enum: ['Point']
+        enum: ["Point"],
       },
       coordinates: {
         type: [Number],
-        index: '2dsphere'
+        index: "2dsphere",
       },
-      formattedAddress: String
     },
-    id: String
+    id: String,
     // bookings: {
     //     type: [mongoose.SchemaTypes.ObjectId],
     //     ref: 'Booking'
@@ -46,24 +46,26 @@ const locationSchema = new mongoose.Schema({
     // },
     // amenities: {
     //     type: String,
-    //     enum: ['Equipment','Barrier-free','Toilet','Electricity','Water connection','Shower','Animals allowed','Grey water disposal','Daily waste disposal','Sink','WLAN','Sauna','Whirlpool','Swimming pool','Washing machine','Surroundings','On a Wieder','On the edge of a field','On the edge of a forest','On a private path','In a courtyard','By a body of water','By a pond','By a river','In a field','In a wood','At a lake','Extras','Seating','Fireplace','Garden','Playground','Farm shop','Battery charging station']
+    //     enum: ['Barrier-free','Lavatory', 'Showers', 'Electricity supply','Water supply','Animals welcome','Grey water disposal','Daily waste disposal','Basin','WiFi','Sauna','Whirlpool','Swimming pool','Washing machine', 'On a private path','In a courtyard','By a body of water','By a pond','By a river','On a field','In the woods','Outdoor seating area','Fireplace','Garden','Playground','Basic supplies available to purchase','Battery charging station']
     // }
     //TODO Add reviews when we reach that feature
-}, { timestamps: true })
+  },
+  { timestamps: true }
+);
 
 // Geocode & create location
-locationSchema.pre('save', async function(next) {
-    const loc = await geocoder.geocode(this.address);
-    console.log(loc)
-    this.location = {
-      type: 'Point',
-      coordinates: [loc[0].longitude, loc[0].latitude],
-      formattedAddress: loc[0].formattedAddress
-    };
-  
-    // Do not save address
-    this.address = undefined;
-    next();
-  });
+locationSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  console.log(loc);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+  };
 
-export const Location = mongoose.model('location', locationSchema)
+  // Do not save address
+  this.address = undefined;
+  next();
+});
+
+export const Location = mongoose.model("location", locationSchema);
