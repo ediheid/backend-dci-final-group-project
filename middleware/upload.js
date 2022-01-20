54767ff969fb65b2fdcb33aa17ac4bb3e0cda23e
Marc Lopez <marclopeztorrent@gmail.com>
2022-01-20 14:54:04 +0100

    configured coudinary in the upload middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,11 +1,21 @@
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
+cloudinary.v2.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "uploads",
     },
     filename: function (req, file, cb) {
+        console.log("upload filename function")
         cb(null, uuidv4().slice(0,8)+"." + file.originalname.split(".").pop());
     }
 });  
